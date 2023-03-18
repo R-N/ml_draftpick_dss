effba803f02c83064c51d1864c1dc58f0d16b017
Muhammad Rizqi Nur <rizqinur2010@gmail.com>
2023-03-18 22:29:18 +0700

    Fix extension splitting

--- ml_draftpick_dss/parsing/util.py
+++ ml_draftpick_dss/parsing/util.py
@@ -39,15 +39,17 @@ def loop_every_n(arr, n):
     for i in range(len(arr)//n):
         yield arr[i*n:n+i*n]
 
+def split_extension(path):
+    return path.rsplit(".", maxsplit=2)
 
 def inference_save_path(save_dir, feature, infered_class, relpath, index=0):
-    file_name = relpath.replace("/", "_").rsplit(".", 2)
+    file_name = split_extension(relpath.replace("/", "_"))
     file_name = f"{file_name[0]}_{index}.{file_name[1]}"
     return os.path.join(save_dir, feature, infered_class, file_name)
 
 def read_save_path(save_dir, feature, read, relpath, index=0):
-    file_name = relpath.replace("/", "_").rsplit(".", 2)
-    file_name = f"{read}_{file_name[0]}_{index}.{file_name[1]}"
+    file_name = split_extension(relpath.replace("/", "_"))
+    file_name = f"{read}_{file_name[0]}_{index}.{file_name[-1]}"
     return os.path.join(save_dir, feature, file_name)
 
 def listify(x):
@@ -60,8 +62,8 @@ def save_inference(obj, path_factory, feature):
     img_key = f"{feature}_img"
     pair = list(zip(listify(obj[feature]), listify(obj[img_key])))
     for i, (inference, img) in enumerate(pair):
-        read_path = path_factory(feature, inference, obj["file"], index=i)
-        cv2.imwrite(read_path, rgb2bgr(img))
+        path = path_factory(feature, inference, obj["file"], index=i)
+        cv2.imwrite(path, rgb2bgr(img))
 
 def mkdir(path):
     path = Path(path)
