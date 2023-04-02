a7ca0b259cf768c1db39081ff6036588317b9c95
Muhammad Rizqi Nur <rizqinur2010@gmail.com>
2023-04-02 13:42:06 +0700

    Update model.py

--- ml_draftpick_dss/predicting/tf_onehot/model.py
+++ ml_draftpick_dss/predicting/tf_onehot/model.py
@@ -5,6 +5,7 @@ from ..transformer import ResultPredictorModel as _ResultPredictorModel
 class ResultPredictorModel(_ResultPredictorModel):
     def __init__(self, d_input, *args, **kwargs):
         super().__init__(d_input, *args, **kwargs)
+        self.d_input = d_input
 
     def summary(self, batch_size=32, dtype=torch.float):
         return summary(
