02410d6a1cb8fc4a9033f635b1884da1d9babb8b
Muhammad Rizqi Nur <rizqinur2010@gmail.com>
2023-04-02 15:16:02 +0700

    Update study.py

--- ml_draftpick_dss/predicting/tf_onehot/study.py
+++ ml_draftpick_dss/predicting/tf_onehot/study.py
@@ -1,5 +1,5 @@
 from ..transformer.study import study as _study
-from ..predictor import ResultPredictor
+from .predictor import ResultPredictor
 
 def study(datasets, d_input, *args, predictor=ResultPredictor, **kwargs):
     return _study(datasets, d_input, *args, predictor=predictor, **kwargs)
