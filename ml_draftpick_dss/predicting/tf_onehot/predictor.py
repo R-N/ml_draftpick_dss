325cb076b7940c3b3cc37d9196e3267232808cd3
Muhammad Rizqi Nur <rizqinur2010@gmail.com>
2023-04-02 13:48:37 +0700

    Update predictor.py

--- ml_draftpick_dss/predicting/tf_onehot/predictor.py
+++ ml_draftpick_dss/predicting/tf_onehot/predictor.py
@@ -1,1 +1,6 @@
-from ..transformer.predictor import ResultPredictor
+from ..transformer.predictor import _ResultPredictor
+from .model import ResultPredictorModel
+
+class ResultPredictor(_ResultPredictor):
+    def __init__(self, *args, model=ResultPredictorModel, **kwargs)
+        super().__init__(*args, model=model, **kwargs)
