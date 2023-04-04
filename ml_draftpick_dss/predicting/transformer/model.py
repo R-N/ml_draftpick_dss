a3f51065b981d43a9cce49477a6b3fcde8649fef
Muhammad Rizqi Nur <rizqinur2010@gmail.com>
2023-04-04 19:59:25 +0700

    Update model.py

--- ml_draftpick_dss/predicting/transformer/model.py
+++ ml_draftpick_dss/predicting/transformer/model.py
@@ -108,7 +108,7 @@ class ResultPredictorModel(nn.Module):
     
     def pos_encode(self, x):
         if self.pos_encoder:
-            x = x * math.sqrt(self.d_input)
+            x = x * math.sqrt(self.d_embed)
             x = self.pos_encoder(x)
         return x
 
